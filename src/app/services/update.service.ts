64f38024eae7a2155d2fd1c94e0c69e34dcc1632
SondreB <sondre@outlook.com>
2024-11-21 13:32:12 +0100

    Add some logging for updates

--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -2,24 +2,25 @@ import { Injectable, signal } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class UpdateService {
-    updateAvailable = signal(false);
+  updateAvailable = signal(false);
 
-    constructor(private swUpdate: SwUpdate) {
-        if (this.swUpdate.isEnabled) {
-            this.swUpdate.versionUpdates.subscribe((event) => {
-                if (event.type === 'VERSION_READY') {
-                    this.updateAvailable.set(true);
-                }
-            });
+  constructor(private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          console.log('New version available');
+          this.updateAvailable.set(true);
         }
+      });
     }
+  }
 
-    updateNow() {
-        this.swUpdate.activateUpdate().then(() => {
-            document.location.reload();
-        });
-    }
+  updateNow() {
+    this.swUpdate.activateUpdate().then(() => {
+      document.location.reload();
+    });
+  }
 }
